e085f7384349c5d858e3f983843f43dfe485d6c5
Tamir112 <72105487+Tamir112@users.noreply.github.com>
2021-03-12 22:39:01 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,25 @@
+-----------------------------------------------------------
+----          Police Rack and Unrack script            ----
+----             Made by Tamir112#5345                 ----
+----        For more scripts and updates Join:         ----
+----              discord.gg/bDPaRDeBKJ                ----
+----                                                   ----
+-----------------------------------------------------------
+
+
+----              To find the weapon hashs use:              ----
+---- https://wiki.gtanet.work/index.php?title=Weapons_Models ----
+
+
+----------- Configure the script to your liking below -----------
+config 				  = {}
+
+-- First weapon used with the rack/unrack command --
+config.weapon1 = -2084633992 -- weapon hash, default set to carbine rifle.
+config.weapon1name = "AR-15" --Name of the gun being Unracked.
+
+-- second weapon used with the rack1/unrack1 command --
+config.weapon2 = 487013001 --weapon hash, default set to pump shotgun.
+config.weapon2name = "Pump Shotgun" --Name of the gin being Unracked.
+
+
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+-----------------------------------------------------------
+----          Police Rack and Unrack script            ----
+----             Made by Tamir112#5345                 ----
+----        For more scripts and updates Join:         ----
+----              discord.gg/bDPaRDeBKJ                ----
+----                                                   ----
+-----------------------------------------------------------
+
+
+fx_version 'bodacious'
+game 'gta5'
+
+client_scripts {
+	'config.lua',
+	'client.lua'
+}
